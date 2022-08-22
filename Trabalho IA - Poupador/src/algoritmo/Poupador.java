29eb42462c9f42820eaa41b5adc172940c911b43
Andres <Afmc07@hotmail.com>
2022-08-22 14:21:24 -0300

    bug de pastilha de poder removido

--- Trabalho IA - Poupador/src/algoritmo/Poupador.java
+++ Trabalho IA - Poupador/src/algoritmo/Poupador.java
@@ -1,13 +1,13 @@
 package algoritmo;
 
 import java.awt.Point;
-import java.util.Arrays;
 
 public class Poupador extends ProgramaPoupador {
 
 	int wall = 1;
 	int coin = 4;
 	int bank = 3;
+	int power_pellet = 5;
 		
 	@Override
 	public int acao() {
@@ -54,16 +54,17 @@ public class Poupador extends ProgramaPoupador {
 			return 3;
 		}
 
-		if (coin == vision[21] && (vision[16] != wall || vision[16] != bank)) {
+		if (coin == vision[21] && (vision[16] != wall && vision[16] != bank && vision[16] != power_pellet)) {
+			System.out.println(vision[21]+" "+vision[16]);
 			System.out.println("Down");
 			return 2;
-		} else if (coin == vision[2] && (vision[7] != wall || vision[7] != bank)) {
+		} else if (coin == vision[2] && (vision[7] != wall && vision[7] != bank && vision[7] != power_pellet)) {
 			System.out.println("Up");
 			return 1;
-		} else if ((coin == vision[0] || coin == vision[1] || coin == vision[5] || coin == vision[14] || coin == vision[19] || coin == vision[20] || coin == vision[10]) && (vision[11] != wall || vision[11] != bank)) {
+		} else if ((coin == vision[0] || coin == vision[1] || coin == vision[5] || coin == vision[14] || coin == vision[19] || coin == vision[20] || coin == vision[10]) && (vision[11] != wall && vision[11] != bank && vision[11] != power_pellet)) {
 			System.out.println("Left");
 			return 4;
-		} else if ((coin == vision[3] || coin == vision[4] || coin == vision[9] || coin == vision[18] || coin == vision[22] || coin == vision[23] || coin == vision[13]) && (vision[12] != wall || vision[12] != bank)) {
+		} else if ((coin == vision[3] || coin == vision[4] || coin == vision[9] || coin == vision[18] || coin == vision[22] || coin == vision[23] || coin == vision[13]) && (vision[12] != wall && vision[12] != bank && vision[12] != power_pellet)) {
 			System.out.println("Right");
 			return 3;
 		}

9ffe4f0d92baf6ae3974c56b42713d9d1dd012d7
jprtenorio00 <jp-tenorio@hotmail.com>
2022-08-22 17:39:31 -0300

    again

--- Trabalho IA - Poupador/src/algoritmo/Poupador.java
+++ Trabalho IA - Poupador/src/algoritmo/Poupador.java
@@ -1,17 +1,12 @@
 package algoritmo;
 
-<<<<<<< HEAD
 //import java.awt.Point;
-=======
-import java.awt.Point;
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 
 public class Poupador extends ProgramaPoupador {
 
 	// Olfato
 	int wall = 1;
 	int bank = 3;
-<<<<<<< HEAD
 	int coin = 4;
 	int power = 5;
 
@@ -20,60 +15,24 @@ public class Poupador extends ProgramaPoupador {
 	public final int baixo = 2;
 	public final int direita = 3;
 	public final int esquerda = 4;
-=======
-	int power_pellet = 5;
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 
 	@Override
 	public int acao() {
 		int[] vision = this.sensor.getVisaoIdentificacao();
-<<<<<<< HEAD
 		// int wallet = this.sensor.getNumeroDeMoedas();
 		// Point position = this.sensor.getPosicao();
 		// Point posBank;
 		
-=======
-		int wallet = this.sensor.getNumeroDeMoedas();
-		Point position = this.sensor.getPosicao();
-		Point posBank;
-
-		if (wallet >= 15) {
-			if (vision[7] == bank || vision[2] == bank) {
-				posBank = position;
-				System.out.println(posBank);
-				return 1;
-			} else if (vision[16] == bank || vision[21] == bank) {
-				posBank = position;
-				System.out.println(posBank);
-				return 2;
-			}else if (vision[11] == bank || vision[10] == bank) {
-				posBank = position;
-				System.out.println(posBank);
-				return 4;
-			} else if (vision[12] == bank || vision[13] == bank) {
-				posBank = position;
-				System.out.println(posBank);
-				return 3;
-			}
-		}
-		// Integer[] visionCopy = Arrays.stream(vision).boxed().toArray(Integer[]::new);
-		// int coin = Arrays.asList(visionCopy).indexOf(coin);
-
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 		// 0  1  2  3  4
 		// 5  6  7  8  9
 		// 10 11 [] 12 13
 		// 14 15 16 17 18
 		// 19 20 21 22 23
-<<<<<<< HEAD
 		
 		// Visualizando o banco e pegando sua posição
 		
 
 		// Entrada para as direções adjacentes
-=======
-
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 		if (vision[7] == coin) {
 			return cima;
 		} else if (vision[16] == coin) {
@@ -84,7 +43,6 @@ public class Poupador extends ProgramaPoupador {
 			return esquerda;
 		}
 
-<<<<<<< HEAD
 		// Entrada para as direções na diagonal
 		if (vision[6] == coin) {
 			if ((vision[11] != wall) && (vision[11] != bank) && (vision[11] != power)) {
@@ -122,31 +80,15 @@ public class Poupador extends ProgramaPoupador {
 
 		// Entrada para as direções na extremidade
 		if (coin == vision[21] && (vision[16] != wall && vision[16] != bank && vision[16] != power)) {
-=======
-		if (coin == vision[21] && (vision[16] != wall && vision[16] != bank && vision[16] != power_pellet)) {
-			System.out.println(vision[21]+" "+vision[16]);
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 			System.out.println("Down");
 			return 2;
-<<<<<<< HEAD
 		} else if (coin == vision[2] && (vision[7] != wall && vision[7] != bank && vision[7] != power)) {
-=======
-		} else if (coin == vision[2] && (vision[7] != wall && vision[7] != bank && vision[7] != power_pellet)) {
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 			System.out.println("Up");
 			return 1;
-<<<<<<< HEAD
 		} else if ((coin == vision[0] || coin == vision[1] || coin == vision[5] || coin == vision[14] || coin == vision[19] || coin == vision[20] || coin == vision[10]) && (vision[11] != wall && vision[11] != bank && vision[11] != power)) {
-=======
-		} else if ((coin == vision[0] || coin == vision[1] || coin == vision[5] || coin == vision[14] || coin == vision[19] || coin == vision[20] || coin == vision[10]) && (vision[11] != wall && vision[11] != bank && vision[11] != power_pellet)) {
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 			System.out.println("Left");
 			return 4;
-<<<<<<< HEAD
 		} else if ((coin == vision[3] || coin == vision[4] || coin == vision[9] || coin == vision[18] || coin == vision[22] || coin == vision[23] || coin == vision[13]) && (vision[12] != wall && vision[12] != bank && vision[12] != power)) {
-=======
-		} else if ((coin == vision[3] || coin == vision[4] || coin == vision[9] || coin == vision[18] || coin == vision[22] || coin == vision[23] || coin == vision[13]) && (vision[12] != wall && vision[12] != bank && vision[12] != power_pellet)) {
->>>>>>> branch 'master' of https://github.com/jprtenorio00/ia-unifor2022.git
 			System.out.println("Right");
 			return 3;
 		}
